b6438efd79246d84ab60bf3bb68f1de746c86036
Reishi Mitani <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2020-12-20 20:51:24 +0900

    remove footer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -88,13 +88,7 @@ const Home: NextPage = () => {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        Random footer
       </footer>
     </div>
   )
